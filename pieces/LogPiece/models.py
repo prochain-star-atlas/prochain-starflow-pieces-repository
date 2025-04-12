44d59e19dfde9ebf5212db95d26c5e4d228c712b
Alexandre Lazar <alexandre.lazar@gmail.com>
2025-04-12 11:15:26 +0200

    fixed namings

--- pieces/LogPiece/models.py
+++ pieces/LogPiece/models.py
@@ -3,13 +3,6 @@ from enum import Enum
 from typing import List, Optional
 from datetime import datetime, time, date
 
-
-class InputEnum(str, Enum):
-    option1 = "option1"
-    option2 = "option2"
-    option3 = "option3"
-
-
 class InputModel(BaseModel):
     """
     LogPiece Input Model
@@ -17,45 +10,6 @@ class InputModel(BaseModel):
     input_str: str = Field(
         default="default value",
         description='Input string to be logged.'
-    )
-    input_int: int = Field(
-        default=10,
-        description='Input integer to be logged.'
-    )
-    input_float: float = Field(
-        default=10.5,
-        description='Input float to be logged.'
-    )
-    input_bool: bool = Field(
-        default=False,
-        description='Input boolean to be logged.'
-    )
-    input_enum: InputEnum = Field(
-        default=InputEnum.option1,
-        description='Input enum to be logged.'
-    )
-    input_date: date = Field(
-        default="2023-01-01",
-        description='Input date to be logged.'
-    )
-    input_time: time = Field(
-        default="16:20:00",
-        description='Input time to be logged.',
-    )
-    input_datetime: datetime = Field(
-        default="2023-01-01T16:20:00",
-        description='Input datetime to be logged.'
-    )
-    input_array: List[str] = Field(
-        default=["default_1", "default_2", "default_3"],
-        description='Input array to be logged.'
-    )
-    input_code: str = Field(
-        default="print('Hello world!')",
-        description='Input code to be logged.',
-        json_schema_extra={
-            'widget': "codeeditor",
-        }
     )
 
 
@@ -65,37 +19,4 @@ class OutputModel(BaseModel):
     """
     output_log: str = Field(
         description='All values logged.'
-    )
-    output_str: Optional[str] = Field(
-        description='Output string logged.'
-    )
-    output_int: Optional[int] = Field(
-        description='Output integer logged.'
-    )
-    output_float: Optional[float] = Field(
-        description='Output float logged.'
-    )
-    output_bool: Optional[bool] = Field(
-        description='Output boolean logged.'
-    )
-    output_enum: Optional[str] = Field(
-        description='Output enum logged.'
-    )
-    output_date: date = Field(
-        description='Output date logged.'
-    )
-    output_time: time = Field(
-        description='Output time logged.',
-    )
-    output_datetime: datetime = Field(
-        description='Output datetime logged.'
-    )
-    output_array: List[str] = Field(
-        description='Output array logged.'
-    )
-    output_code: str = Field(
-        description='Output code logged.',
-        json_schema_extra={
-            'widget': "codeeditor",
-        }
     )
--- pieces/LogPiece/piece.py
+++ pieces/LogPiece/piece.py
@@ -6,44 +6,13 @@ from pathlib import Path
 class LogPiece(BasePiece):
 
     def piece_function(self, input_data: InputModel):
+
         # Log inputs
-        msg = f"""## Log Piece: \n\n
-Input string: {input_data.input_str}\n
-Input integer: {input_data.input_int}\n
-Input float: {input_data.input_float}\n
-Input boolean: {input_data.input_bool}\n
-Input array: {input_data.input_array}\n
-Input enum: {input_data.input_enum}\n
-Input date: {input_data.input_date}\n
-Input time: {input_data.input_time}\n
-Input datetime: {input_data.input_datetime}\n
-Input code: {input_data.input_code}\n
-"""
+        msg = f"""msg: {input_data.input_str}"""
         self.logger.info(msg)
-
-        # Write log file
-        self.logger.info("Writing log file to shared_storage...")
-        file_path = str(Path(self.results_path)/"log.txt")
-        with open(file_path, "w") as f:
-            f.write(msg)
-
-        # Set display result
-        self.display_result = {
-            "file_type": "txt",
-            "file_path": file_path
-        }
 
         # Return output
         return OutputModel(
             output_log=msg,
-            output_str=input_data.input_str,
-            output_int=input_data.input_int,
-            output_float=input_data.input_float,
-            output_bool=input_data.input_bool,
-            output_enum=input_data.input_enum,
-            output_array=input_data.input_array,
-            output_date=input_data.input_date,
-            output_time=input_data.input_time,
-            output_datetime=input_data.input_datetime,
-            output_code=input_data.input_code
+            output_str=input_data.input_str
         )
