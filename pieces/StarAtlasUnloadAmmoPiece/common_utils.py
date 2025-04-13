e798e5c7aae215f6edc09b91bc30fa5c8e56e01e
Alexandre Lazar <alexandre.lazar@gmail.com>
2025-04-13 13:08:49 +0200

    fixed common utils

--- pieces/StarAtlasUnloadAmmoPiece/common_utils.py
+++ pieces/StarAtlasUnloadAmmoPiece/common_utils.py
@@ -1,15 +1,13 @@
 from typing import Any
-from starflow.base_piece import BasePiece
-from .models import FleetStatusEnum, InputModel, OutputModel
 from time import sleep
 import time as timew
 import requests
-from keycloak import KeycloakOpenID
-import os
 import json
+import logging
 
+logger = logging.getLogger(__name__)
 
-def retry_put_request(self, url_formated, bearer_token):
+def retry_put_request(url_formated, bearer_token):
     headers = {"Authorization": "Bearer " + bearer_token['access_token']}
     retries = 0
     success = False
@@ -19,12 +17,12 @@ def retry_put_request(self, url_formated, bearer_token):
             response_raw = requests.put(url_formated, headers=headers, verify=False, timeout=120)
             response_raw_json = response_raw.json()
             success = True
-            self.logger.info("Successfully executed !")
+            logger.info("Successfully executed !")
             json_formatted_str = json.dumps(response_raw_json, indent=2)
-            self.logger.info(json_formatted_str)           
+            logger.info(json_formatted_str)           
             
         except Exception as e:
-            self.logger.error(f"Waiting {wait_time} secs and re-trying...")
+            logger.error(f"Waiting {wait_time} secs and re-trying...")
             timew.sleep(wait_time)
             retries += 1
 
